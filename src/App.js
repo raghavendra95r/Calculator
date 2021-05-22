a05da1cf953632ed1dfbce0f3f01e1b12db5895b
raghavendra95r <rrgowda95@gmail.com>
2021-05-22 12:19:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import CalculatorResult from './components/CalculatorResult/CalculatorResult';
+import Keypad from './components/Keypad/Keypad';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      result : ""
+    }
+  }
+  onClick = button =>{
+    if(button === "Ans"){
+      this.calculate()
+    }
+    else if (button === "C"){
+      this.reset()
+    }
+    else if(button === "CE"){
+      this.backspace()
+    }
+    else{
+      this.setState({
+        result: this.state.result+ button
+      })
+    }
+  }
+
+  calculate = () => {
+   try{
+     this.setState({
+       result : (eval(this.state.result) || "") + ""
+     })
+    }
+    catch (e) {
+      this.setState({result : "error"})
+    }
+  }
+
+  reset =() => {
+    this.setState({result : ""})
+  }
+
+  backspace= () => {
+    this.setState({result : this.state.result.slice(0,-1)})
+  }
+
+  render() {
+    return (
+      <div className="calculatorBody">
+        
+        <h1>Calculator</h1>
+     
+        
+        <CalculatorResult result={this.state.result}/>
+        <Keypad onClick={ this.onClick} />
+        
+      </div>
+    )
+  }
 }
-
-export default App;
